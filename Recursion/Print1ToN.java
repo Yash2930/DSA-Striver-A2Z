e8008c84d62a637f2e2fcf355b5d23e42fa6154e
Yash2930 <Yashv1062@gmail.com>
2026-05-30 07:03:40 +0530

    Solved Print 1 To N Times

---
+++ Recursion/Print1ToN.java
@@ -0,0 +1,16 @@
+public class Print1ToN {
+    public static void printToN(int n){
+  
+        if(n<=0) return;
+       
+        printToN(n-1);
+        System.out.println(n);
+
+    }
+
+
+
+    public static void main(String[] args) {
+        printToN(5);
+    }
+}
