818f141b61f77c0e022fff509a572559e944028c
Yash2930 <Yashv1062@gmail.com>
2026-05-30 06:53:17 +0530

    Solved Print Name N Times using recursion

---
+++ Recursion/PrintNameNTimes.java
@@ -0,0 +1,17 @@
+public class PrintNameNTimes {
+    
+    public static void printName(int n){
+     if(n<=0) return;
+
+     System.out.println("Yash");
+     
+     printName(n-1);
+
+    }
+
+    public static void main(String[] args) {
+        printName(5);
+    }
+
+
+}
